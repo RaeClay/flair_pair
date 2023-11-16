1e91a0788778d1a69e7782f8caf70e34a13dad82
Rae Clayton <44236925+RaeClay@users.noreply.github.com>
2023-11-15 22:18:59 -0500

    added more pairings to repository :cake:

--- lib/repositories/pairing_repository.dart
+++ lib/repositories/pairing_repository.dart
@@ -8,28 +8,68 @@ class PairingRepository {
         alcoholName: 'Wine',
         pairingDescription: 'A classic combination for wine enthusiasts.',
       ),
-    ],
-    [
       PairingModel(
-        foodName: 'Pizza',
-        alcoholName: 'Beer',
-        pairingDescription: 'An all-time favorite pairing for a casual meal.',
+        foodName: 'Chocolate',
+        alcoholName: 'Wine',
+        pairingDescription: 'Indulge in the sweet harmony of chocolate and wine.',
+      ),
+      PairingModel(
+        foodName: 'Grilled Vegetables',
+        alcoholName: 'White Wine',
+        pairingDescription: 'Lighten up with grilled veggies and a crisp white wine.',
       ),
     ],
     [
       PairingModel(
         foodName: 'Steak',
-        alcoholName: 'Wine',
-        pairingDescription: 'Another classic combination for wine enthusiasts.',
+        alcoholName: 'Red Wine',
+        pairingDescription: 'A hearty steak with a robust red wine.',
+      ),
+      PairingModel(
+        foodName: 'Pizza',
+        alcoholName: 'Beer',
+        pairingDescription: 'An all-time favorite pairing for a casual meal.',
+      ),
+      PairingModel(
+        foodName: 'Pretzels',
+        alcoholName: 'Beer',
+        pairingDescription: 'Pretzels go well with a cold beer.',
       ),
     ],
     [
       PairingModel(
-        foodName: 'Pretzel',
-        alcoholName: 'Beer',
-        pairingDescription: 'A timeless combination of salty and hoppy.',
+        foodName: 'Sushi',
+        alcoholName: 'Sake',
+        pairingDescription: 'Enhance your sushi experience with a glass of sake.',
+      ),
+      PairingModel(
+        foodName: 'Tempura',
+        alcoholName: 'Sake',
+        pairingDescription: 'Crispy tempura paired with a smooth sake.',
+      ),
+      PairingModel(
+        foodName: 'Teriyaki Chicken',
+        alcoholName: 'Sake',
+        pairingDescription: 'Teriyaki chicken and sake, a delightful combination.',
       ),
     ],
+    [
+      PairingModel(
+        foodName: 'Buffalo Wings',
+        alcoholName: 'IPA',
+        pairingDescription: 'Kick up the heat with spicy wings and a hoppy IPA.',
+      ),
+      PairingModel(
+        foodName: 'Tacos',
+        alcoholName: 'Tequila',
+        pairingDescription: 'Salty, spicy tacos to complement Tequila.',
+      ),
+      PairingModel(
+        foodName: 'Oysters',
+        alcoholName: 'Chardonnay',
+        pairingDescription: 'Savor the briny goodness of oysters with a buttery Chardonnay.',
+      ),
+    ]
   ]; // end of list of lists (_allPairings) for hardcoded data
 
   List<List<PairingModel>> getAllPairings() {
