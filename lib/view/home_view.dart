bd5e7325cd1484b7ab88fa0f26a6fd33efcb1c27
jakegolddd <36602861+jakegolddd@users.noreply.github.com>
2023-11-16 14:49:01 -0500

    Updated homeview carousel and buttons

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -15,8 +15,8 @@ class HomePage extends StatelessWidget {
         decoration: BoxDecoration(
           gradient: RadialGradient(
             colors: [
-              Colors.grey[800]!, // Lighter color in the center
-              Colors.deepOrangeAccent, // Darker color towards the edges
+              Colors.grey[800]!,
+              Colors.deepOrangeAccent,
             ],
             center: Alignment.center,
             radius: 1.0,
@@ -24,28 +24,29 @@ class HomePage extends StatelessWidget {
         ),
         child: Column(
           children: <Widget>[
-            SizedBox(height: 20), // Space between app bar and carousel
+            SizedBox(height: 20),
             Expanded(
+              flex: 2, // Adjusted flex for carousel
               child: CarouselSlider(
                 options: CarouselOptions(
-                  height: 450,
-                  viewportFraction: 0.8, // 80% of the viewport width
-                  enlargeCenterPage: true, // Enlarge the central page
+                  height: 350, // Increased height of the carousel
+                  viewportFraction: 0.8,
+                  enlargeCenterPage: true,
                 ),
                 items: [1, 2, 3].map((i) {
                   return Builder(
                     builder: (BuildContext context) {
                       return Container(
-                        margin: EdgeInsets.symmetric(horizontal: 5.0), // Margin for spacing
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
                         decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(20), // Rounded corners
+                          borderRadius: BorderRadius.circular(20),
                           boxShadow: [
                             BoxShadow(
                               color: Colors.black.withOpacity(0.3),
                               spreadRadius: 2,
                               blurRadius: 5,
-                              offset: Offset(0, 3), // Shadow position
+                              offset: Offset(0, 3),
                             ),
                           ],
                         ),
@@ -59,6 +60,48 @@ class HomePage extends StatelessWidget {
                     },
                   );
                 }).toList(),
+              ),
+            ),
+            Expanded(
+              flex: 3, // Increased flex for buttons to take more vertical space
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Implement action for Tips
+                        },
+                        child: Text('Tips', style: TextStyle(color: Colors.black)),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          padding: EdgeInsets.symmetric(vertical: 100),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30), // Rounded edges
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10), // Space between buttons
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Implement action for For you
+                        },
+                        child: Text('For you', style: TextStyle(color: Colors.black)),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          padding: EdgeInsets.symmetric(vertical: 100),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30), // Rounded edges
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
