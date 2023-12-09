903567a5c600cec9627c96297e576c4a76d78b71
JohnAmick1 <79718965+JohnAmick1@users.noreply.github.com>
2023-12-08 20:35:36 -0500

    carousel slides pretty much done, might change the font down the line

--- lib/customwidgets/carousel_items.dart
+++ lib/customwidgets/carousel_items.dart
@@ -4,7 +4,7 @@ List<Widget> carouselItems = [
   Container(
     decoration: BoxDecoration(
       image: DecorationImage(
-        image: AssetImage('assets/images/winter-background.jpg'),
+        image: AssetImage('assets/images/winterbackground2.jpg'),
         fit: BoxFit.cover,
       ),
       borderRadius: BorderRadius.circular(20),
@@ -35,7 +35,7 @@ List<Widget> carouselItems = [
                       fontSize: 20,
                       fontFamily: 'RubikBubbles',
                       fontWeight: FontWeight.w900,
-                      color: Colors.white,
+                      color: const Color(0xff9E6E61),
 
                     ),
                     overflow: TextOverflow.ellipsis, // Prevents text from overflowing
@@ -50,23 +50,147 @@ List<Widget> carouselItems = [
               ],
             ),
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 23),
           Container(
             padding: const EdgeInsets.all(10), // Add padding around the text
-            decoration: BoxDecoration(
-              color: const Color(0xff679876), //background color
-              borderRadius: BorderRadius.circular(10), // Rounded edges
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 4,
-                  offset: Offset(0, 3), // changes position of shadow
+
+            child: Text(
+              'Celebrate the cold weather with a pairing we love - a classic mulled wine with a rich, spiced gingerbread. The warmth of the wine complements the sweet and spicy flavors of the gingerbread, perfect for cozy winter evenings.',
+              textAlign: TextAlign.center, // Center align the text
+              style: TextStyle(
+                fontFamily: 'RubikBubbles',
+                fontSize: 16,
+                color: Colors.brown,
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+  ),
+
+
+
+
+  Container(
+    decoration: BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage('assets/images/taco-background.jpg'),
+        fit: BoxFit.cover,
+      ),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'The Classics',
+            style: TextStyle(
+              fontFamily:'RubikBubbles',
+              fontSize: 40,
+              color: const Color(0xffEBB314),
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Row(
+              mainAxisSize: MainAxisSize.min, // To align the contents in the center
+              children: [
+                Flexible( // Wrap the Text widget with Flexible
+                  child: Text(
+                    'tacos and a marg',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'RubikBubbles',
+                      fontWeight: FontWeight.w900,
+                      color: const Color(0xffEBB314),
+
+                    ),
+                    overflow: TextOverflow.ellipsis, // Prevents text from overflowing
+                  ),
+                ),
+                SizedBox(width: 10), // Provides some space between text and image
+                Image.asset(
+                  'assets/images/margarita.png', // Path to your image file
+                  width: 40, // Adjust the size as needed
+                  height: 40, // Adjust the size as needed
                 ),
               ],
             ),
+          ),
+          SizedBox(height: 20),
+
+          Container(
+            padding: const EdgeInsets.all(10), // Add padding around the text
+
             child: Text(
-              'Celebrate the cold weather with a pairing we love - a classic mulled wine with a rich, spiced gingerbread. The warmth of the wine complements the sweet and spicy flavors of the gingerbread, perfect for cozy winter evenings.',
+              'Does this ever get old? No matter the taco filling or the flavor of margartia, this'
+                  ' classic combo never fails in brining a sigh of relief.',
+              textAlign: TextAlign.center, // Center align the text
+              style: TextStyle(
+                fontFamily: 'RubikBubbles',
+                fontSize: 16,
+                color: const Color(0xffEBB314),
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+  ),
+
+  Container(
+    decoration: BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage('assets/images/bbqbackground.jpg'),
+        fit: BoxFit.cover,
+      ),
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'BBQ and Brews',
+            style: TextStyle(
+              fontFamily: 'RubikBubbles',
+              fontSize: 40,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisSize: MainAxisSize.min, // To align the contents in the center
+            children: [
+              Flexible( // Wrap the Text widget with Flexible
+                child: Text(
+                  'pulled pork and amber ale',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'RubikBubbles',
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white,
+                  ),
+                  overflow: TextOverflow.ellipsis, // Prevents text from overflowing
+                ),
+              ),
+              SizedBox(width: 10), // Provides some space between text and image
+              Image.asset(
+                'assets/images/beer-bottle.png', // Path to your image file
+                width: 40, // Adjust the size as needed
+                height: 40, // Adjust the size as needed
+              ),
+            ],
+          ),
+          SizedBox(height: 23),
+          Container(
+            padding: const EdgeInsets.all(10), // Add padding around the text
+            child: Text(
+              'Smoky pulled pork meets the caramel smoothness of amber ale - the ale\'s subtle bitterness cuts through the richness of the pork, making each bite and sip a perfectly balanced flavor dance.',
               textAlign: TextAlign.center, // Center align the text
               style: TextStyle(
                 fontFamily: 'RubikBubbles',
@@ -78,37 +202,5 @@ List<Widget> carouselItems = [
         ],
       ),
     ),
-  ),
-  Container(
-    margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 7.0),
-    decoration: BoxDecoration(
-      color: Colors.blue, // Different color for each container
-      borderRadius: BorderRadius.circular(20),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black.withOpacity(0.3),
-          spreadRadius: 2,
-          blurRadius: 2,
-          offset: const Offset(0, 3),
-        ),
-      ],
-    ),
-    child: const Center(child: Text('Item 2', style: TextStyle(fontSize: 24, color: Colors.white))),
-  ),
-  Container(
-    margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 7.0),
-    decoration: BoxDecoration(
-      color: Colors.blue, // Different color for each container
-      borderRadius: BorderRadius.circular(20),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black.withOpacity(0.3),
-          spreadRadius: 2,
-          blurRadius: 2,
-          offset: const Offset(0, 3),
-        ),
-      ],
-    ),
-    child: const Center(child: Text('Item 3', style: TextStyle(fontSize: 24, color: Colors.white))),
   ),
 ];
