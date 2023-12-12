07d777bf9d07e02a6e23e9b4f9ccf00f95794e71
JohnAmick1 <79718965+JohnAmick1@users.noreply.github.com>
2023-12-11 22:44:21 -0500

    tips and where to pair button render overflow fix

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,12 +1,10 @@
-import 'package:flair_pair/packages.dart';
 
+import 'package:flair_pair/packages.dart';
 
 class HomePage extends StatelessWidget {
   final BottomNavBarVM viewModel = BottomNavBarVM();
 
-
   HomePage({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +32,7 @@ class HomePage extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             Expanded(
-              flex: 4, // Maintained flex for carousel
+              flex: 4,
               child: CarouselSlider(
                 options: CarouselOptions(
                   height: 350,
@@ -60,6 +58,7 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+            const SizedBox(height: 20),
             Expanded(
               flex: 2,
               child: Padding(
@@ -67,119 +66,23 @@ class HomePage extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    SizedBox(
-                      width: 210, // Adjust width as needed
-                      height: 130, // Adjust height as needed
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const OnboardingPage1()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF0A1E24),
-                          padding: EdgeInsets.zero, // Set padding to zero
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                        child: Stack(
-                          children: [
-                            const Align(
-                              alignment: Alignment.topLeft, // Aligns 'Tips' text to the top left
-                              child: Padding(
-                                padding: EdgeInsets.only(top: 10, left: 20), // Padding for the 'Tips' text
-                                child: Text(
-                                  'Tips',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20, // Font size for 'Tips'
-                                    fontFamily: 'ArchivoBlack', // Specified font family
-                                  ),
-                                ),
-                              ),
-                            ),
-                            const Center( // Centered text
-                              child: Text(
-                                "How to pair like a pro...",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 14,
-                                  fontFamily: 'ArchivoBlack', // Font size for the central text
-                                ),
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.bottomRight, // Aligns the image to the bottom right
-                              child: Padding(
-                                padding: const EdgeInsets.all(10), // Padding for the image
-                                child: Image.asset(
-                                  'assets/images/tips-lightbulb.png', // Path to your image
-                                  width: 35, // Set your desired width
-                                  height: 35, // Set your desired height
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                    CustomButton(
+                      title: 'Tips',
+                      icon: Image.asset('assets/images/tips-lightbulb.png', width: 35, height: 35),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const OnboardingPage1()),
+                        );
+                      },
                     ),
-                    const SizedBox(width: 10),
-                    SizedBox(
-                      width: 210, // Adjust the width as needed
-                      height: 130, // Adjust the height as needed
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Implement action for 'For you'
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF0A1E24),
-                          padding: EdgeInsets.zero, // Set padding to zero for consistency
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                        child: const Stack(
-                          children: [
-                            Align(
-                              alignment: Alignment.topLeft, // Aligns text to the top left
-                              child: Padding(
-                                padding: EdgeInsets.only(top: 10, left: 20), // Padding for the text
-                                child: Text(
-                                  'Where to Pair',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20, // Font size
-                                    fontFamily: 'ArchivoBlack', // Specified font family
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Center( // Centered text (Replace with your desired text)
-                              child: Text(
-                                "Find eateries nearby!",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 14,
-                                  fontFamily: 'ArchivoBlack', // Font size for the central text
-                                ),
-                              ),
-                            ),
-                            Align(
-                              alignment: Alignment.bottomRight, // Position for an image or icon
-                              child: Padding(
-                                padding: EdgeInsets.all(10), // Padding for the image/icon
-                                child: Icon(
-                                  dining, // Example icon, replace with your desired icon or image
-                                  color: Colors.white,
-                                  size: 35,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                    const SizedBox(width: 10), // Spacing between buttons
+                    CustomButton(
+                      title: 'Where to Pair',
+                      icon: Icon(Icons.map, color: Colors.white, size: 35),
+                      onPressed: () {
+                        // Implement action for 'For you'
+                      },
                     ),
                   ],
                 ),
@@ -189,6 +92,53 @@ class HomePage extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: BottomNavBar(viewModel: viewModel, context: context),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final Widget icon;
+  final VoidCallback onPressed;
+
+  CustomButton({required this.title, required this.icon, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    const double maxButtonWidth = 140.0; // Fixed maximum width for each button
+    const double maxButtonHeight = 150.0;
+
+    return Container(
+      width: maxButtonWidth,
+      height: maxButtonHeight,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          primary: const Color(0xFF0A1E24),
+          padding: EdgeInsets.symmetric(vertical: 15),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon,
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'ArchivoBlack',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
