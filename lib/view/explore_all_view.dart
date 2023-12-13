1b17cbabde17cebf18f88621f50705a837de5641
JohnAmick1 <79718965+JohnAmick1@users.noreply.github.com>
2023-12-12 19:01:59 -0500

    find all pairings has navbar sections

--- lib/view/explore_all_view.dart
+++ lib/view/explore_all_view.dart
@@ -1,51 +1,58 @@
-import 'package:flair_pair/packages.dart';
 
+import 'package:flair_pair/packages.dart';
 class ExploreAllPairingsPage extends StatefulWidget {
   @override
   _ExploreAllPairingsPageState createState() => _ExploreAllPairingsPageState();
 }
 
-class _ExploreAllPairingsPageState extends State<ExploreAllPairingsPage> {
+class _ExploreAllPairingsPageState extends State<ExploreAllPairingsPage> with SingleTickerProviderStateMixin {
   final PairingRepository _pairingRepository = PairingRepository();
   Map<String, List<PairingModel>> _sortedPairings = {};
+  TabController? _tabController;
 
   @override
   void initState() {
     super.initState();
     _sortedPairings = _pairingRepository.getAllPairings();
+    _tabController = TabController(length: _sortedPairings.keys.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Explore All Pairings'),
-      ),
-      body: ListView.builder(
-        itemCount: _sortedPairings.keys.length,
-        itemBuilder: (context, index) {
-          String alcoholType = _sortedPairings.keys.elementAt(index);
-          List<PairingModel> pairings = _sortedPairings[alcoholType] ?? [];
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  alcoholType,
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              ...pairings.map((pairing) => ListTile(
-                title: Text('${pairing.foodName} with ${pairing.alcoholName}'),
-                subtitle: Text(pairing.pairingDescription),
-              )).toList(),
-            ],
-          );
-        },
+    return DefaultTabController(
+      length: _sortedPairings.keys.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Explore All Pairings'),
+          bottom: TabBar(
+            controller: _tabController,
+            isScrollable: true,
+            tabs: _sortedPairings.keys.map((String category) => Tab(text: category)).toList(),
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: _sortedPairings.keys.map((String category) {
+            List<PairingModel> pairings = _sortedPairings[category] ?? [];
+            return ListView.separated(
+              itemCount: pairings.length,
+              itemBuilder: (context, index) {
+                PairingModel pairing = pairings[index];
+                return ListTile(
+                  title: Text('${pairing.foodName} with ${pairing.alcoholName}'),
+                  subtitle: Text(pairing.pairingDescription),
+                );
+              },
+              separatorBuilder: (context, index) => Divider(), // Adding a Divider here
+            );
+          }).toList(),
+        ),
       ),
     );
   }
